127e9b77f0dfb5ad92355d5fe98167532f291869
Rafael Solís U <rafasu8@gmail.com>
2017-04-24 16:04:37 -0500

    Update pushArray.js

--- Basic_Javascript/pushArray.js
+++ Basic_Javascript/pushArray.js
@@ -1,1 +1,16 @@
+/*
+An easy way to append data to the end of an array is via the push() function.
+
+.push() takes one or more parameters and "pushes" them onto the end of the array.
+*/
+
 //Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]); 
+// ourArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+myArray.push(["dog", 3]) ;
