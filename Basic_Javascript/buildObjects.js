bb5af14fc477d60f81ccd5faaffde58be82bedce
Rafael Solís U <rafasu8@gmail.com>
2017-04-25 22:31:50 -0500

    Update buildObjects.js

--- Basic_Javascript/buildObjects.js
+++ Basic_Javascript/buildObjects.js
@@ -1,2 +1,23 @@
+//
+//Objects are similar to arrays, except that instead of using indexes to access and modify their data, 
+//you access the data in objects through what are called properties.
+//Objects are useful for storing data in a structured way, and can represent real world objects, like a dog
+
 //Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name" : "Javidu",
+  "legs": 4,
+  "tails": 1 ,
+  "friends": ["everyone"]
+  
+};
 
