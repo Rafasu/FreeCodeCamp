7b87e134dd97f89a2b1638131d2dec09353607a2
Rafael Solís U <rafasu8@gmail.com>
2017-04-26 20:50:33 -0500

    Update privateProperty.js

--- Object_Oriented/privateProperty.js
+++ Object_Oriented/privateProperty.js
@@ -1,2 +1,52 @@
 //Make Object Properties private.
+/*
+We can also create private properties and private methods, which aren't accessible from outside the object.
 
+To do this, we create the variable inside the constructor using the var keyword we're familiar with, instead of creating it as a 
+property of this.
+
+This is useful for when we need to store information about an object but we want to control how it is used by outside code.
+
+For example, what if we want to store the speed our car is traveling at but we only want outside code to be able to modify it 
+by accelerating or decelerating, so the speed changes in a controlled way?
+*/
+//Example
+
+var Car = function() {
+  // this is a private variable
+  var speed = 10;
+
+  // these are public methods
+  this.accelerate = function(change) {
+    speed += change;
+  };
+
+  this.decelerate = function() {
+    speed -= 5;
+  };
+
+  this.getSpeed = function() {
+    return speed;
+  };
+};
+
+var Bike = function() {
+
+  // Only change code below this line.
+  //Private
+  var gear; 
+  
+  //Public
+  this.getGear = function(){
+    return this.gear ;
+  };
+    
+  this.setGear = function(gear){
+      this.gear = gear ;
+  };
+  
+};
+
+var myCar = new Car();
+
+var myBike = new Bike();
