df81586c7f16d089372fece63c05502fb6ee8493
Rafael Solís U <rafasu8@gmail.com>
2017-04-26 20:19:34 -0500

    Update constructor.js

--- Object_Oriented/constructor.js
+++ Object_Oriented/constructor.js
@@ -1,1 +1,35 @@
+/*
+A constructor function is given a capitalized name to make it clear that it is a constructor.
+
+Here's an example of a constructor function:
+
+var Car = function() {
+  this.wheels = 4;
+  this.engines = 1;
+  this.seats = 5;
+};
+In a constructor the this variable refers to the new object being created by the constructor. So when we write,
+
+  this.wheels = 4;
+
+inside of the constructor we are giving the new object it creates a property called wheels with a value of 4.
+
+You can think of a constructor as a description for the object it will create.
+*/
+
 //Example
+
+var Car = function() {
+  this.wheels = 4;
+  this.engines = 1;
+  this.seats = 5;
+};
+
+// Only change code below this line.
+
+var MotorBike = function() {
+  this.wheels = 2 ;
+  this.engines= 1 ;
+  this.seats = 1 ;
+
+};
