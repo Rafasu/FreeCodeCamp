c96b32f87b67aca812fcf81066eb559b92813434
Rafael Solís U <rafasu8@gmail.com>
2017-04-30 19:56:03 -0500

    Update confirmEnding.js

--- Basic_Algorithm_Scripting/easy/confirmEnding.js
+++ Basic_Algorithm_Scripting/easy/confirmEnding.js
@@ -1,1 +1,25 @@
 //Confirm the ending
+/*
+Check if a string (first argument, str) ends with the given target string (second argument, target).
+*/
+
+function confirmEnding(str, target) {
+  // "Never give up and good luck will find you."
+  // -- Falcor
+   
+  var compare = '' ;
+  for(var i = str.length -1 ; i >= 0 ; i--){
+    compare = str.substring(i, str.length) ;
+    if(compare === target){
+      return true ;
+    }
+    if(str[i] === ' '){
+      break ;
+    }
+  }
+ 
+  return false;
+}
+
+confirmEnding("Bastian", "n");
+
