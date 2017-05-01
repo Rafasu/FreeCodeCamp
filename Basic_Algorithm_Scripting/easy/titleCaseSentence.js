8e803511ade0e9cf4121b28918e8bf2da754f5ea
Rafael Solís U <rafasu8@gmail.com>
2017-04-30 20:04:55 -0500

    Update titleCaseSentence.js

--- Basic_Algorithm_Scripting/easy/titleCaseSentence.js
+++ Basic_Algorithm_Scripting/easy/titleCaseSentence.js
@@ -1,1 +1,33 @@
 //Title Case Sentence.
+
+function titleCase(str) {
+  var upper = false ;
+  var charStr = '' ;
+  
+  charStr = str.split('');
+  
+  //Traverse the array to convert all letters to lowercase.
+  for(var i = 0 ; i < charStr.length ; i++ ){
+   charStr[i] = charStr[i].toLowerCase() ;  
+  }
+  
+  //Convert the first letter uppercase.
+  charStr[0] = charStr[0].toUpperCase() ;
+  
+  //Conver the first letter of each word to uppercase.
+  for(i = 1 ; i < charStr.length ; i++){
+     if(charStr[i] === ' ' ){
+       upper = true ;
+     }
+    else if(upper){
+      charStr[i] = charStr[i].toUpperCase() ;
+      upper =  false ;
+    }
+  }
+  
+  //Join the array.
+  charStr = charStr.join('') ;
+  return charStr;
+}
+
+titleCase("I'm a little tea pot");
