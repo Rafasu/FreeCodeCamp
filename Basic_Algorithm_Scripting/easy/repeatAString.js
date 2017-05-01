2965138292b17a9ffe07529e44898f47de523caf
Rafael Solís U <rafasu8@gmail.com>
2017-04-30 19:54:32 -0500

    Update repeatAString.js

--- Basic_Algorithm_Scripting/easy/repeatAString.js
+++ Basic_Algorithm_Scripting/easy/repeatAString.js
@@ -1,1 +1,20 @@
+/*
+Repeat a given string (first argument) num times (second argument). Return an empty string if num is not a positive number.
+*/
+
 //Repeat a string repeat a string.
+function repeatStringNumTimes(str, num) {
+  // repeat after me
+  var repeat = '' ;
+  //If num is 0, return an empty string.
+  if(num === 0 ){
+    return repeat ;
+  }
+  while(num > 0){
+    repeat += str ;
+    num-- ;
+  }
+  return repeat;
+}
+
+repeatStringNumTimes("abc", 3);
