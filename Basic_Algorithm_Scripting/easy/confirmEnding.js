0494032ab48086cd9056e15cacf08777e360c9f0
Rafael Solís U <rafasu8@gmail.com>
2017-04-30 19:58:01 -0500

    Update confirmEnding.js

--- Basic_Algorithm_Scripting/easy/confirmEnding.js
+++ Basic_Algorithm_Scripting/easy/confirmEnding.js
@@ -6,18 +6,20 @@ Check if a string (first argument, str) ends with the given target string (secon
 function confirmEnding(str, target) {
   // "Never give up and good luck will find you."
   // -- Falcor
-   
   var compare = '' ;
+  
   for(var i = str.length -1 ; i >= 0 ; i--){
     compare = str.substring(i, str.length) ;
     if(compare === target){
       return true ;
     }
+    //Exits the cycle after readign all the last word.
     if(str[i] === ' '){
       break ;
     }
   }
  
+  //If the target was not on the last word, return false.
   return false;
 }
 
