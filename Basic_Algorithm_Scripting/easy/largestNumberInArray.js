b6cd97cda0d528c354b137edb645890023d06ccf
Rafael Solís U <rafasu8@gmail.com>
2017-04-30 20:02:41 -0500

    Update largestNumberInArray.js

--- Basic_Algorithm_Scripting/easy/largestNumberInArray.js
+++ Basic_Algorithm_Scripting/easy/largestNumberInArray.js
@@ -1,2 +1,21 @@
 //Return largest in Arrays.
 
+function largestOfFour(arr) {
+  // You can do this!
+  var largest = [] ;
+  var max = 0 ;
+  
+  for(var i = 0; i < 4 ; i++){
+    for(var j  = 0 ; j < arr[i].length; j++ ){
+      if(arr[i][j] > max){
+        max = arr[i][j] ;
+      }
+    }
+    largest.push(max) ;
+    max = 0 ;
+  }
+  
+  return largest;
+}
+
+largestOfFour([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]) ;
